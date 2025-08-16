7be283c8ce3246e09e262b7bfa1d9c26c5034d41
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-08-16 12:57:27 +0400

    implement update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,10 +3,18 @@ import { ClerkProvider } from "@clerk/clerk-expo";
 import { Slot } from "expo-router";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { StatusBar } from "expo-status-bar";
+import { Alert } from "react-native";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
 export default function RootLayout() {
+  if (!publishableKey) {
+    Alert.alert("Configuration error", "Clerk key is missing");
+    throw new Error("Missing EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY");
+  }
+
   return (
-    <ClerkProvider tokenCache={tokenCache}>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
       <SafeScreen>
         <Slot />
       </SafeScreen>
